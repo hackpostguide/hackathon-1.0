20596a63b14aef888868f89f53ac66179b77300c
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-13 10:34:39 -0400

    chore: Update page title and description for HG Hackathon

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,50 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
-      <section className="min-h-[80vh] bg-slate-900">
-        <div className="mx-auto p-10 text-center max-w-5xl bg-red-100">
+      <section className="min-h-[80vh] bg-neutral-950 flex items-center text-background">
+        <div className="mx-auto p-10 text-center max-w-5xl">
+          <p>
+            Spice up this summer with the very first 
+          </p>
           <h1>
             Hackpost Guide Hackathon
           </h1>
+
+          <Button asChild>
+            <Link href="https://hg-hackathon.devpost.com" target="_blank" rel="noopener noreferrer">
+              Register now
+            </Link>
+          </Button>
+
         </div>
 
       </section>
 
-      <p>Brought to you by hackpost</p>
+      {/* Stats - prizes, schedule */}
+      <section>
+
+      </section>
+
+      {/* How to register */}
+      <section>
+
+      </section>
+
+      {/* FAQ */}
+      <section>
+      
+      </section>
+
+      {/* Sponsors */}
+      <section>
+
+      </section>
+
+      <p>Brought to you by Hackpost Guide</p>
     </main>
   );
 }
