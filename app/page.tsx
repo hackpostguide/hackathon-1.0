a27401bdb609e8a0cd5e1ad61481068a8c269698
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-14 13:05:34 -0400

    chore: Update landing page with Hackpost Guide Hackathon details

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="text-center">
-      <Nav />
       {/* Hero Section */}
-      <section className="min-h-[80vh] bg-neutral-950 flex items-center text-background">
-        <div className="mx-auto p-10 max-w-3xl">
-          <h3 className="text-xl sm:text-2xl text-muted-foreground font-normal">
+      <section className="min-h-[80vh] py-5 bg-neutral-950 flex-col items-center text-background">
+        <Nav />
+        <div className="mx-auto p-10 max-w-5xl">
+          <h2 className="text-2xl sm:text-3xl text-muted-foreground font-normal">
             Spice up your summer with the very first 
-          </h3>
+          </h2>
           <div className="mt-4 mb-6 flex flex-col sm:flex-row justify-center">
-            <h1 className="text-highlight">
+            <h1 className="text-highlight text-5xl sm:text-7xl font-black">
               Hackpost Guide&nbsp;
             </h1>
-            <h1 className="">Hackathon</h1>
+            <h1 className="text-7xl font-black">Hackathon</h1>
           </div>
           <p className="text-lg sm:text-xl mb-10">
             A virtual hackathon organized by Hackpost Guide. Join us for 2 weeks of hacking, learning, and fun!
@@ -34,24 +34,24 @@ export default function Home() {
       {/* Prizes */}
       <section className="px-10">
         <div className="py-10 max-w-5xl mx-auto">
-          <h2>Prizes</h2>
-          <p>Prizes will be announced soon!</p>
+          <h1>Prizes and Schedule</h1>
+          <p>The event will be held from <b>8/25 to 9/7</b>. Prizes will be announced soon. Make sure to Join our discord server for the latest announcements.</p>
         </div>
       </section>
 
       {/* Schedule */}
-      <section className="px-10">
+      {/* <section className="px-10">
         <div className="py-10 max-w-5xl mx-auto">
-          <h2>Schedule</h2>
+          <h1>Schedule</h1>
           <p>Coming soon!</p>
         </div>
-      </section>
+      </section> */}
 
       {/* How to register */}
       <section className="flex items-center justify-center p-5">
         <Card className="max-w-5xl w-full bg-slate-600 text-background py-20">
-            <CardTitle>
-              <h2 className="text-3xl font-bold mb-4">How to register</h2>
+            <CardTitle className="text-4xl font-bold mb-4">
+              How to register
             </CardTitle>
             <CardContent>
               Click the button below to register on Devpost. Next, join our Discord server to find teammates, collaborate, and receive announcements!
@@ -62,7 +62,7 @@ export default function Home() {
 
       {/* FAQ */}
       <section className="px-10">
-        <h2 className="pt-10">Frequently Asked Questions</h2>
+        <h1 className="pt-10">Frequently Asked</h1>
         <div className="max-w-5xl mx-auto">
           <FAQ />
         </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Logo } from "@/components/Icons";
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className='max-w-4xl'>
-        
-    </div>
+    <header className="sticky top-0 z-50 backdrop-blur">
+        <div className="flex max-w-5xl mx-auto h-16 items-center justify-between px-4 md:px-6">
+          <Link className="flex items-center gap-1 text-lg" href="https://hackpost.guide" target="_blank" rel="noopener noreferrer">
+            <Logo />
+            <p className="ml-1 font-bold text-background">Hackpost</p>
+            <p className="text-background">Guide</p>
+          </Link>
+        </div>
+    </header>
   )
 }
