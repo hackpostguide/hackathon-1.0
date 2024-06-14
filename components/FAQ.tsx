1122226ca9a0d991307ba60b5f8e4dfaea984b9e
Alex <53095140+CometBlazer@users.noreply.github.com>
2024-06-14 13:18:12 -0400

    chore: Update FAQ component with Hackpost Guide information

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion'
+import Link from 'next/link'
 
 export const FAQ = () => {
   return (
     <div>
         <Accordion type="single" collapsible className="w-full">
             <AccordionItem value="item-1">
-                <AccordionTrigger>Is it free?</AccordionTrigger>
+                <AccordionTrigger>What is a Hackathon?</AccordionTrigger>
                 <AccordionContent>
-                    <p>Yes!</p>
+                    <p>A hackathon is a design sprint-like event in which computer programmers and others involved in software development, including graphic designers, interface designers, project managers, and others, often including domain experts, collaborate intensively on software projects.</p>
                 </AccordionContent>
             </AccordionItem>
-            {/* <AccordionItem value="item-2">
-                <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionItem value="item-2">
+                <AccordionTrigger>What is Hackpost Guide?</AccordionTrigger>
                 <AccordionContent>
-                    <p>Yes!</p>
+                    <p>Hackpost Guide is a community of hackers, developers, and tech enthusiasts who are passionate about learning and building cool projects. We host hackathons, workshops, and other events to help you grow your skills and connect with others. For more information, check out </p>
+                    <Link className='' href={"https://hackpost.guide"}>https://hackpost.guide</Link> 
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-                <AccordionTrigger>Is it animated?</AccordionTrigger>
+                <AccordionTrigger>Who can attend?</AccordionTrigger>
+                <AccordionContent>
+                    <p>Anyone can attend, and no coding experience is necessary! Non-STEM majors, first-time hackers, and beginner coders are welcome and encouraged to join us!</p>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+                <AccordionTrigger>Is it free to attend?</AccordionTrigger>
                 <AccordionContent>
                     <p>Yes!</p>
                 </AccordionContent>
-            </AccordionItem> */}
+            </AccordionItem>
+            <AccordionItem value="item-5">
+                <AccordionTrigger>What languages can I use?</AccordionTrigger>
+                <AccordionContent>
+                    
+                </AccordionContent>
+            </AccordionItem>
         </Accordion>
     </div>
   )
